fa5e34acb8a5e6ba958c22fa70125881d7eed82b
Ryoga-Nagai <105972246+RyogaNagaiVogue@users.noreply.github.com>
2023-11-06 21:30:57 +0900

    For Portfolio
    For Portfolio

---
+++ Birth_from_Egg(MediaArt_Project)/Assets/EggFaller.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggFaller : MonoBehaviour
+{
+    //タマゴをゆっくりおろしていく
+    //一定値に来たらxとzを所定の範囲にランダムに設置してリスポーンする
+    public float xl = 0f, xh = 0f, zl = 0f, zh = 0f;
+    public float respownY = 0f, warpPointY = 0f;
+
+    private System.Random r;
+
+    void Start()
+    {
+        r = new System.Random();//ランダムの生成
+    }
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.y -= 0.0005f;//ゆっくり落ちていく
+
+        if (pos.y < warpPointY)
+        {//ワープポイントに来たらx,zを変えてリスポーンポイントにワープ
+            pos.y = respownY;
+            pos.x = (float)r.NextDouble() * (xh - xl) + xl;
+            pos.z = (float)r.NextDouble() * (zh - zl) + zl;
+        }
+
+        transform.position = pos;
+
+
+    }
+}
